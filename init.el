15d3207b9974c5f76599fb3b8de063ff0c6eba31
dagda1 <dagda1@scotalt.net>
2015-01-01 07:45:07 +0000

    remove % parens

--- init.el
+++ init.el
@@ -269,14 +269,6 @@
 
 (setq ag-reuse-buffers 't)
 
-(global-set-key "%" 'match-paren)
-(defun match-paren (arg)
-  "Go to the matching paren if on a paren; otherwise insert %."
-  (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-        (t (self-insert-command (or arg 1)))))
-
 (global-set-key "\M-/" 'hippie-expand)
 ;; Append result of evaluating previous expression (Clojure):
 (defun cider-eval-last-sexp-and-append ()

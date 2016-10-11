20f47fc8f2ba49642c1aa805259d3c2359eaf662
dagda1 <dagda1@scotalt.net>
2016-10-11 08:14:47 +0100

    set comment style for web-mode

--- init.el
+++ init.el
@@ -82,16 +82,23 @@
 
 (require 'web-mode)
 
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js?$" . web-mode))
-
 (defun my-web-mode-hook ()
   "web-mode settings"
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-auto-quoting nil))
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js?$" . web-mode))
+
+(setq-default web-mode-comment-formats (remove '("javascript" . "/*") web-mode-comment-formats))
+(add-to-list 'web-mode-comment-formats '("javascript" . "//"))
+
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
 
 (define-minor-mode pair-helpers-minor-mode
   "This mode contains little helpers for js developement"

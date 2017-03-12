9be3e0cb1818de4af2c5b21ab60e0709f6e5879f
dagda1 <dagda1@scotalt.net>
2017-03-12 20:30:13 +0000

    jsx

--- init.el
+++ init.el
@@ -140,15 +140,31 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (setq web-mode-enable-auto-quoting nil))
+  (setq web-mode-enable-auto-quoting nil)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-css-colorization t))
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js[x]?$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'"   . web-mode))
 
 (setq web-mode-content-types-alist
   '(("jsx" . "\\.js[x]?\\'")))
+
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "js")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 (define-minor-mode pair-helpers-minor-mode
   "This mode contains little helpers for js developement"
